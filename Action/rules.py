2603978bcfe66b9cad535d68b82df74a4c8e91d0
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2025-03-22 09:30:51 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -6,12 +6,12 @@ def get_text(url):
     file = parts[-1]
     with requests.get(url, stream= True) as r:
         if r.status_code == 200:
-            with open("./Rules/file", "wb") as f:
+            with open("./Rules/"+file, "wb") as f:
                 for chunk in r.iter_content(chunk_size=4096):
                     if chunk:
                         f.write(chunk)
             time.sleep(0.1)
-            with open("./Rules/file", "r",encoding='utf-8') as f:
+            with open("./Rules/"+file, "r",encoding='utf-8') as f:
                 return f.read().strip()
         else:
             sys.exit(0)
