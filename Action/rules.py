430e4a73fdd44ae4e0d98db59b26cf56d83f6ea8
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2025-03-22 12:18:49 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -73,6 +73,7 @@ reject_text = '\n'.join(sorted(reject_set))
 with open("./Rules/reject.list", "w",encoding='utf-8') as f:
     f.write(reject_text)
 get_url(REJECT_URL[2], 'reject.plugin')
+del reject_set,reject_text
 
 tmp_set = set([i for i in get_text(PROXY_URL[0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
 proxy_set = set()
@@ -90,6 +91,7 @@ LEN_proxy = len(proxy_set)
 proxy_text = '\n'.join(sorted(proxy_set))
 with open("./Rules/proxy.list", "w",encoding='utf-8') as f:
     f.write(proxy_text)
+del proxy_set,proxy_text
 
 direct_set = set()
 for item in DIRECT_URL[0]:
@@ -101,11 +103,25 @@ for i in tmp_set:
     else:
         j = 'DOMAIN,' + i
     direct_set.add(j)
-tmp_set.clear()
+del tmp_set
 for item in DIRECT_URL[1]:
     direct_set.update([i for i in get_text(item).split("\n") if not (i.startswith('#') or i.startswith('!'))])
 LEN_direct = len(direct_set)
 direct_text = '\n'.join(sorted(direct_set))
 with open("./Rules/direct.list", "w",encoding='utf-8') as f:
     f.write(direct_text)
+del direct_set,direct_text
+my_stat = []
+with open("./Rules/stat", "r",encoding='utf-8') as f:
+    my_stat.extend([i[i.rindex(' ')+1:] for i in f.read().strip().split("\n") if not i.startswith('#')])
+LEN_reject0,LEN_proxy0,LEN_direct0,LEN_total= int(my_stat[0]),int(my_stat[1]),int(my_stat[2]),int(my_stat[3]))
+STR_stat = f'#{(datetime.now().astimezone(timezone(timedelta(hours=8)))).strftime("%Y/%m/%d %H:%M:%S")}(UTC/GMT+08:00)\n\
+reject rules({LEN_reject0}{"+" if LEN_reject-LEN_reject0 >= 0 else "-"}{abs(LEN_reject-LEN_reject0)}): {LEN_reject}\n\
+proxy rules({LEN_proxy0}{"+" if LEN_proxy-LEN_proxy0 >= 0 else "-"}{abs(LEN_proxy-LEN_proxy0)}): {LEN_proxy}\n\
+direct rules({LEN_social0}{"+" if LEN_direct-LEN_direct0 >= 0 else "-"}{abs(LEN_direct-LEN_direct0)}): {LEN_direct}\n\
+total rules({LEN_total}{"+" if LEN_reject+LEN_proxy+LEN_direct-LEN_total >= 0 else "-"}{abs(LEN_reject+LEN_proxy+LEN_direct-LEN_total)}): {LEN_reject+LEN_proxy+LEN_direct}'
+with open("./stat", "w",encoding='utf-8') as f:
+    f.write(STR_stat)
+del my_stat,STR_stat,LEN_reject,LEN_proxy,LEN_direct,LEN_reject0,LEN_proxy0,LEN_direct0,LEN_total
+
 
