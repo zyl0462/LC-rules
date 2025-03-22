77266f08686391078da3cae13e7efc10b86da6a0
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2025-03-22 10:56:31 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -8,9 +8,9 @@ def get_text(url):
                 for chunk in r.iter_content(chunk_size=4096):
                     if chunk:
                         f.write(chunk)
-        time.sleep(0.1)
-        with open("./Rules/tmp", "r",encoding='utf-8') as f:
-            return f.read().strip()
+            time.sleep(0.1)
+            with open("./Rules/tmp", "r",encoding='utf-8') as f:
+                return f.read().strip()
         else:
             sys.exit(0)
 ############################################################  
