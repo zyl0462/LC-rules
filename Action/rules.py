a3aee67875ae3637824854b88635bd28e2279134
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2025-03-22 10:33:52 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -2,13 +2,19 @@ import requests,sys,time
 from datetime import datetime,timezone,timedelta
 
 def get_text(url):
-    file = url.split("/")[-1]
     with requests.get(url, stream= True) as r:
         if r.status_code == 200:
-            with open("./Rules/" + file, "wb") as f:
+            with open("./Rules/tmp", "wb") as f:
                 for chunk in r.iter_content(chunk_size=4096):
                     if chunk:
                         f.write(chunk)
+        time.sleep(0.1)
+        with open("./Rules/tmp", "r",encoding='utf-8') as f:
+            return f.read().strip()
         else:
             sys.exit(0)
+AD_URL = ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Advertising/Advertising.list",
+          "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/Advertising/Advertising_Domain.list",
+          "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/Advertising/Advertising_MITM.plugin"
+         )
 
