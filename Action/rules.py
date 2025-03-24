3e5317cc20357d53f23047e6b8fa043fa9bf2342
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2025-03-24 20:01:58 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -24,10 +24,8 @@ def get_url(url, file):
             sys.exit(0)
 ############################################################  
 ############################################################
-REJECT_URL = ('https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/Advertising/Advertising_Domain.list',
-          'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Advertising/Advertising.list',
-          'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/Advertising/Advertising_MITM.plugin'
-         )
+REJECT_URL = ("https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt",
+             "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt")
 PROXY_URL = ('https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/Proxy/Proxy_Domain.list',
              ('https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/Proxy/Proxy.list',
              'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/AppleTV/AppleTV.list',
@@ -55,24 +53,18 @@ DIRECT_URL = (('https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/r
               'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/ByteDance/ByteDance.list')
              )
 
-tmp_set = set([i for i in get_text(REJECT_URL[0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
+tmp_set = set([i for i in get_text(RULES_URL[0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
+tmp_set .update([i[2:-1] for i in get_text(RULES_URL[1]).split("\n") if (i.startswith('||') and i.endswith('^'))])
+
 reject_set = set()
 for i in tmp_set:
-    j = ''
-    if i.startswith('.'):
-        j = 'DOMAIN,' + i[1:]
-    else:
-        j = 'DOMAIN,' + i
+    j = 'DOMAIN,' + i
     reject_set.add(j)
 tmp_set.clear()
-tmp_set = set([i for i in get_text(REJECT_URL[1]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
-reject_set.update(tmp_set)
 LEN_reject = len(reject_set)
-tmp_set.clear()
 reject_text = '\n'.join(sorted(reject_set))
 with open("./Rules/reject.list", "w",encoding='utf-8') as f:
     f.write(reject_text)
-get_url(REJECT_URL[2], 'reject.plugin')
 del reject_set,reject_text
 
 tmp_set = set([i for i in get_text(PROXY_URL[0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
