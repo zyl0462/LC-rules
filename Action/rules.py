40b7d9d3f04642316c3498030b68b40b8930150f
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2025-03-25 23:33:08 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -54,20 +54,27 @@ DIRECT_URL = (('https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/r
               'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Loon/ByteDance/ByteDance.list')
              )
 
-reject_set = set([i for i in get_text(REJECT_URL[0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
-reject_set.update([i[2:-1] for i in get_text(REJECT_URL[1]).split("\n") if (i.startswith('||') and i.endswith('^'))])
-
+tmp_set = set([i for i in get_text(REJECT_URL[0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
+tmp_set.update([i[2:-1] for i in get_text(REJECT_URL[1]).split("\n") if (i.startswith('||') and i.endswith('^'))])
+reject_set = set()
+j = ''
+for i in tmp_set:
+    if i.startswith('.'):
+        j = 'DOMAIN-SUFFIX,' + i[1:]
+    else:
+        j = 'DOMAIN,' + i
+    reject_set.add(j)
+tmp_set.clear()
 LEN_reject = len(reject_set)
 reject_text = '\n'.join(sorted(reject_set))
 with open("./Rules/reject.list", "w",encoding='utf-8') as f:
     f.write(reject_text)
 del reject_set,reject_text
 
-tmp_set = set()
 for item in PROXY_URL[0]:
     tmp_set.update([i for i in get_text(item).split("\n") if not (i.startswith('#') or i.startswith('!'))])
 proxy_set = set()
-j = ''
+
 for i in tmp_set:
     if i.startswith('.'):
         j = 'DOMAIN-SUFFIX,' + i[1:]
